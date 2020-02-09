2c0818e4feba02fcd3dd5f69813669531e3200bf
Krasimir Todorov <krasimir.c.todorov@gmail.com>
2020-02-09 20:12:25 +0200

    Test change

--- src/com/vsc/strings/Testing.java
+++ src/com/vsc/strings/Testing.java
@@ -10,7 +10,7 @@ public class Testing {
     }
 
     public static String formatLetter(String[] data) {
-        return String.format("%s%n%s%n%s%n%s%n%s", data[0], data[1],
+        return String.format("%s\n%s\n%s\n%s\n%s", data[0], data[1],
                 data[2], data[3], data[4]);
     }
 }
